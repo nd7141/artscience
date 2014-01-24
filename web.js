aeb450685afab007c8658804980477afeb0cf59e
Sergey Ivanov <ivanovserg990@gmail.com>
2014-01-24 14:18:22 +0400

    Replace template on windows

--- web.js
+++ web.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var fs = require('fs');
-var path = require('path');
 var app = express();
 
 app.use(express.logger());
 
-var htmlFile = './index.html';
+var htmlFile = './public/index.html';
 
-app.use(express.static(path.join(__dirname, './assets/')));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
 	var html = fs.readFileSync(htmlFile).toString();
 	res.send(html);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 })
