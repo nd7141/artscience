f52cbaa9671563ffe8744428fb7d39b1545e09bc
Ivanov Sergey <ivanovserg990@gmail.com>
2014-01-24 12:07:53 +0400

    Add reading bootstrap template

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 var app = express();
+
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var htmlFile = './index.html';
+
+app.use(express.static(path.join(__dirname, './assets/')));
+
+app.get('/', function(req,res){
+	var html = fs.readFileSync(htmlFile).toString();
+	res.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+	console.log("Listening on " + port);
+})
